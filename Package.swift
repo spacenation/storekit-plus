515795b958d21ded095d1f88b73786d033b09e94
Alexander Young <alex.buzov@icloud.com>
2021-05-02 17:48:53 -0700

    tvOS support (#24)

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "StoreKitExtensions",
     platforms: [
-         .iOS(.v13), .macOS(.v10_15)
+        .iOS(.v14), .macOS(.v11), .tvOS(.v14)
     ],
     products: [
         .library(name: "StoreKitExtensions", type: .static, targets: ["StoreKitExtensions"])
