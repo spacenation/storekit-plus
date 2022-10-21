7163e89b4af61b4fcb4a4f43e82f4ed0f1401508
Alexander Young <alex.buzov@icloud.com>
2022-10-20 19:56:08 -0700

    Update Package.swift (#26)

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .iOS(.v14), .macOS(.v11), .tvOS(.v14)
     ],
     products: [
-        .library(name: "Storefront", type: .static, targets: ["Storefront"])
+        .library(name: "Storefront", targets: ["Storefront"])
     ],
     targets: [
         .target(name: "Storefront"),
