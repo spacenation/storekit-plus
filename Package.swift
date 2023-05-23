f1b510ede51810d12ded8a5e921cdfba3d197607
Alexander Young <alex.buzov@icloud.com>
2023-05-22 17:11:06 -0700

    storekit plus

--- Package.swift
+++ Package.swift
@@ -2,15 +2,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "Storefront",
+    name: "StoreKitPlus",
     platforms: [
         .iOS(.v15), .macOS(.v12), .tvOS(.v15), .watchOS(.v8)
     ],
     products: [
-        .library(name: "Storefront", targets: ["Storefront"])
+        .library(name: "StoreKitPlus", targets: ["StoreKitPlus"])
     ],
     targets: [
-        .target(name: "Storefront"),
-        .testTarget(name: "StorefrontTests", dependencies: ["Storefront"])
+        .target(name: "StoreKitPlus"),
+        .testTarget(name: "StoreKitPlusTests", dependencies: ["StoreKitPlus"])
     ]
 )
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-## Storefront
+## StoreKitPlus
